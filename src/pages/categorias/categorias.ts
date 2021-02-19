273d435cae6e2dba934662959c522382885cb290
Jeronimo Kulandissa <zemiro014@gmail.com>
2021-02-18 21:05:33 -0300

    Criando interceptor para tratamento de Error

--- src/pages/categorias/categorias.ts
+++ src/pages/categorias/categorias.ts
@@ -32,12 +32,9 @@ export class CategoriasPage {
   ionViewDidLoad() {
     this.categoriaService.findAll()
         .subscribe(response =>{
-            console.log(response);
             this.items = response;
     },
-    error => {
-            console.log(error);
-    });
+    error => { });
 
   }
 }
